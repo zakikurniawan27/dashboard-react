a0b5f60f2eced4d93f13a65ef5b7ae362de7f9b1
Ragil M Rivandi <50643466+immedy@users.noreply.github.com>
2024-11-14 16:15:43 +0800

    first commit

---
+++ src/app/routes.jsx
@@ -0,0 +1,38 @@
+import { lazy } from "react";
+import { Navigate } from "react-router-dom";
+
+import AuthGuard from "./auth/AuthGuard";
+import { authRoles } from "./auth/authRoles";
+
+import Loadable from "./components/Loadable";
+import MatxLayout from "./components/MatxLayout/MatxLayout";
+import sessionRoutes from "./views/sessions/session-routes";
+import materialRoutes from "app/views/material-kit/MaterialRoutes";
+
+// E-CHART PAGE
+const AppEchart = Loadable(lazy(() => import("app/views/charts/echarts/AppEchart")));
+// DASHBOARD PAGE
+const Analytics = Loadable(lazy(() => import("app/views/dashboard/Analytics")));
+
+const routes = [
+  { path: "/", element: <Navigate to="dashboard/default" /> },
+  {
+    element: (
+      <AuthGuard>
+        <MatxLayout />
+      </AuthGuard>
+    ),
+    children: [
+      ...materialRoutes,
+      // dashboard route
+      { path: "/dashboard/default", element: <Analytics />, auth: authRoles.admin },
+      // e-chart route
+      { path: "/charts/echarts", element: <AppEchart />, auth: authRoles.editor }
+    ]
+  },
+
+  // session pages route
+  ...sessionRoutes
+];
+
+export default routes;
